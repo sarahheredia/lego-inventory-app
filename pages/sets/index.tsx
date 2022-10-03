977ba0642d6c1903ff30988c698a579a59d6553b
Sarah Heredia <sarah.heredia@procore.com>
2022-10-02 21:05:18 -0400

    add sorting

--- pages/sets/index.tsx
+++ pages/sets/index.tsx
@@ -4,13 +4,20 @@ import {
   Breadcrumbs,
   Card,
   Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
   Grid,
   Link,
+  Radio,
+  RadioGroup,
+  TableSortLabel,
   TextField,
   Typography,
 } from '@mui/material';
 import { LegoSet } from '../../types/LegoSet.d';
 import { useState } from 'react';
+import { orderBy, sortBy } from 'lodash';
 
 type Props = {
   legoSets: Array<LegoSet>;
@@ -18,7 +25,10 @@ type Props = {
 
 export default function SetList({ legoSets }: Props) {
   const [searchText, setSearchText] = useState('');
+  const [sort, setSort] = useState('pieces');
+  const [sortDirection, setSortDirection] = useState<'asc'| 'desc'>('asc');
   const filtered = legoSets.filter(set => set.searchText!.indexOf(searchText) > -1);
+  const sorted = orderBy(filtered, [sort], [sortDirection])
   return (
     <>
       <Head>
@@ -35,15 +45,33 @@ export default function SetList({ legoSets }: Props) {
         </Card>
 
         <Card style={{ border: 'rgb(32, 29, 72) solid 24px', textAlign: 'center', marginTop: '24px', padding: '24px' }}>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '24px' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '24px', marginLeft: '12px', marginRight: '12px' }}>
+            <FormControl>
+              <FormLabel style={{textAlign: 'left'}}>Sort By</FormLabel>
+              <RadioGroup
+                row
+                name="sorting-group"
+                onChange={event => setSort(event.target.value)}
+              >
+                <FormControlLabel value="pieces" control={<Radio />} label="Pieces" checked={sort === 'pieces'} />
+                <FormControlLabel value="series" control={<Radio />} label="Series" checked={sort === 'series'} />
+                <FormControlLabel value="year" control={<Radio />} label="Year" checked={sort === 'year'} />
+                <TableSortLabel
+                  active
+                  direction={sortDirection}
+                  onClick={() => setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')}
+                />
+              </RadioGroup>
+            </FormControl>
+
             <TextField label="Search" variant="outlined" onChange={(event) => setSearchText(event.target.value.toLowerCase())} style={{width: '300px'}} />
           </Box>
 
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center">
-            {!filtered.length ? (
+            {!sorted.length ? (
               <h1>No Results...</h1>
             ) :
-            filtered.map((set: LegoSet) => {
+            sorted.map((set: LegoSet) => {
               return (
                 <Grid item key={set.name}>
                   <a href={`/sets/${set.number}`} style={{ textDecoration: 'none' }}>
