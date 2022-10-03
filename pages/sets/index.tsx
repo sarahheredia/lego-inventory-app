d9eb1fbc0fd2fdb2a926fe35cfbba9b02f62a6f5
Sarah Heredia <sarah.heredia@procore.com>
2022-10-02 23:06:44 -0400

    Add filtering

--- pages/sets/index.tsx
+++ pages/sets/index.tsx
@@ -45,6 +45,10 @@ export default function SetList({ legoSets, photos }: Props) {
   const [searchText, setSearchText] = useState('');
   const [sort, setSort] = useState('pieces');
   const [sortDirection, setSortDirection] = useState<'asc'| 'desc'>('desc');
+  const [areComplete, setAreComplete] = useState(false);
+  const [areBagged, setAreBagged] = useState(false);
+  const [areDisplayed, setAreDisplayed] = useState(false);
+  const [havePhotos, setHavePhotos] = useState(false);
   const photosGroup = groupBy(photos, photo => photo.for);
   const legoSetsWithPhotos: Array<SearchableLegoSet> = legoSets.map(set => {
     const photos = (photosGroup[set.number.toString()] ?? []).map(p => p.url);
@@ -54,7 +58,14 @@ export default function SetList({ legoSets, photos }: Props) {
       photoCount: photos.length,
     };
   });
-  const filtered = legoSetsWithPhotos.filter(set => set.searchText!.indexOf(searchText) > -1);
+  const filtered = legoSetsWithPhotos.filter(set => {
+    const matchesSearch = set.searchText!.indexOf(searchText) > -1;
+    const isComplete = areComplete ? set.complete : true;
+    const isBagged = areBagged ? set.bagged : true;
+    const isDisplayed = areDisplayed ? set.displayed : true;
+    const hasPhotos = havePhotos ? set.photoCount > 0 : true;
+    return matchesSearch && isComplete && isBagged && isDisplayed && hasPhotos;
+  });
   const sorted = orderBy(filtered, [sort], [sortDirection]);
   return (
     <>
@@ -72,7 +83,7 @@ export default function SetList({ legoSets, photos }: Props) {
         </Card>
 
         <Card style={{ border: 'rgb(32, 29, 72) solid 24px', textAlign: 'center', marginTop: '24px', padding: '24px', marginBottom: '24px' }}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '24px', marginLeft: '12px', marginRight: '12px' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginLeft: '12px', marginRight: '12px' }}>
             <FormControl>
               <FormLabel style={{textAlign: 'left'}}>Sort By</FormLabel>
               <RadioGroup
@@ -86,12 +97,28 @@ export default function SetList({ legoSets, photos }: Props) {
                   onClick={() => setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')}
                 />
                 <FormControlLabel value="pieces" control={<Radio />} label="Pieces" checked={sort === 'pieces'} />
+                <FormControlLabel value="complete" control={<Radio />} label="Complete" checked={sort === 'complete'} />
                 <FormControlLabel value="series" control={<Radio />} label="Series" checked={sort === 'series'} />
                 <FormControlLabel value="year" control={<Radio />} label="Year" checked={sort === 'year'} />
                 <FormControlLabel value="photoCount" control={<Radio />} label="Photo Count" checked={sort === 'photoCount'} />
               </RadioGroup>
             </FormControl>
-            <TextField label={`Search ${legoSets.length} sets...`} variant="outlined" onChange={(event) => setSearchText(event.target.value.toLowerCase())} style={{width: '300px'}} />
+            <TextField label={`Search ${filtered.length} sets...`} variant="outlined" onChange={(event) => setSearchText(event.target.value.toLowerCase())} style={{width: '300px'}} />
+          </Box>
+
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '24px', marginLeft: '12px', marginRight: '12px' }}>
+            <FormControl>
+              <FormLabel style={{textAlign: 'left'}}>Filter By</FormLabel>
+              <RadioGroup
+                row
+                name="filtering-group"
+              >
+                <FormControlLabel value="complete" control={<Checkbox onChange={() => setAreComplete(!areComplete)} />} label="Complete Set" checked={areComplete} />
+                <FormControlLabel value="bagged" control={<Checkbox onChange={() => setAreBagged(!areBagged)} />} label="In Bag" checked={areBagged} />
+                <FormControlLabel value="displayed" control={<Checkbox onChange={() => setAreDisplayed(!areDisplayed)} />} label="Displayed" checked={areDisplayed} />
+                <FormControlLabel value="photoCount" control={<Checkbox onChange={() => setHavePhotos(!havePhotos)} />} label="Have Photos" checked={havePhotos} />
+              </RadioGroup>
+            </FormControl>
           </Box>
 
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent="center">
@@ -113,6 +140,7 @@ export default function SetList({ legoSets, photos }: Props) {
                       <Typography>Released: <strong>{set.year}</strong></Typography>
                       <Typography>Pieces: <strong>{set.pieces}</strong></Typography>
                       <Typography>Complete: <strong>{set.complete ? 'Yes' : 'No'}</strong></Typography>
+                      <Typography>Bagged: <strong>{set.bagged ? 'Yes' : 'No'}</strong></Typography>
                       <Typography>Minifigs: <strong>{set.minifigs}</strong></Typography>
                       <Typography>Photos: <strong>{set.photoCount}</strong></Typography>
                     </Card>
