3c090920b4f2bfe8b8d3813182fcf541065f1470
Sarah Heredia <sarah.heredia@procore.com>
2022-10-04 22:37:00 -0400

    Fix useEffect running twice on load

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   serverRuntimeConfig: {
     googleServiceKey: process.env.GOOGLE_SERVICE_KEY,
